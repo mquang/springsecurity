c87138bec864f18b7aa850b9f714ac71f461a16d
mquang <mqfrontendtime@gmail.com>
2024-04-28 16:43:41 +0700

    customize Security Configuration

--- section2/securitydemo/src/main/java/com/mquang/securitydemo/config/ProjectSecurityConfig.java
+++ section2/securitydemo/src/main/java/com/mquang/securitydemo/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.mquang.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,11 +11,22 @@ public class ProjectSecurityConfig {
 
 	@Bean
 	SecurityFilterChain overrideDefaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		/*//Some methods are deprecated and planned to be removed in Spring Security 7
 		http.authorizeHttpRequests()
 			.requestMatchers("/myAccount", "/myBalance").authenticated()
 			.requestMatchers("/notices").permitAll()
 			.and().formLogin()
 			.and().httpBasic();
+		*/
+		
+		//Lambda DSL style
+		http.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/myAccount", "/myBalance").authenticated()
+				.requestMatchers("/notices").permitAll()
+				);
+		http.formLogin(Customizer.withDefaults());
+		http.httpBasic(Customizer.withDefaults());
+		
 		return http.build();
 	}
 }
